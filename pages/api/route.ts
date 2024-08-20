6de30847ccd3ccace7657de35279705b312fa77b
AchelDrinker <99177027+AchelDrinker@users.noreply.github.com>
2024-08-20 12:35:14 +0200

    Add files via upload

---
+++ pages/api/route.ts
@@ -0,0 +1,30 @@
+import { EmailTemplate } from '../../components/EmailTemplate';
+import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next';
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+const emailHandler: NextApiHandler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== 'POST') {
+    return res.status(405).end();
+  }
+
+  const {fullname, email, subject, message } = req.body;
+
+  try {
+    const data = await resend.emails.send({
+      from: "onboarding@resend.dev",
+      to: "ecurie.lizaine@gmail.com",
+      subject: subject,
+      text: message,
+      react: EmailTemplate({ name: fullname, email:email, subject:subject, message: message }),
+    });
+
+    return res.status(200).json(data);
+  } catch (error) {
+    console.error("Erreur d'envoi de l'email: ", error);
+    return res.status(500).json({ error });
+  }
+};
+
+export default emailHandler;
